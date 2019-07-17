a3c81dada13d2bd1113c30b38427a5837f40b2fa
SGATADE <sgatade@gmail.com>
2019-07-17 22:57:19 +0200

    Renamed User to Supplier

--- src/routes/subscriber.js
+++ src/routes/subscriber.js
@@ -2,3 +2,16 @@
  * Subscriber Routes
  * @SG on 2019-07-17, 22:02
  */
+
+ // Libraries
+ const express = require("express");
+
+ // Local
+ const Subscriber = require("../models/subscriber");
+
+ // Route
+ const router = new express.Router();
+
+// Module Exports
+module.exports = router;
+
