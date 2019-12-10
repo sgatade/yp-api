5c1f9619d02ad2d4d0479995cb32651a31b8fe9d
SGATADE <sgtechwork@gmail.com>
2019-12-10 13:29:47 +0100

    Added Status and Mobile props to Subscriber

--- src/models/subscriber.js
+++ src/models/subscriber.js
@@ -1,51 +1,71 @@
 /**
- * Base user model
+ * Subscriber model
  * @SG on 2019-07-17, 21:36
-*/
+ */
 
 const mongoose = require("mongoose");
 const validator = require("validator");
 
-// Schema
-const subscriberSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: true,
-        minlength: 5,
-        maxlength: 50
-    },
-    email: {
-        type: String,
-        validate(value) {
-            if(!validator.isEmail(value)) {
-                throw new Error("Email is invalid!");
-            }
-        }
-    },
-    password: {
-        type: String,
-        minlength: 3
-    },
-    devices: [
-        {
-            deviceId: {
-                type: String
-            },
-            added: {
-                type: Date,
-                default: Date.now
-            },
-            lastUsed: {
-                type: Date
-            }
-        }
-    ]
-}, {
-    timestamps: true
+const Status = Object.freeze({
+    ACTIVE: 'active',
+    INACTIVE: 'inactive',
+    BARRED: 'barred',
+    REMOVED: 'removed'
 });
 
+// Schema
+const subscriberSchema = new mongoose.Schema(
+    {
+        name: {
+            type: String,
+            required: true,
+            minlength: 5,
+            maxlength: 50
+        },
+        email: {
+            type: String,
+            validate(value) {
+                if (!validator.isEmail(value)) {
+                    throw new Error("Email is invalid!");
+                }
+            }
+        },
+        status: {
+            type: String,
+            enum: Object.values(Status)
+        },
+        mobile: {
+            type: String,
+            maxlength: 20
+        },
+        password: {
+            type: String,
+            minlength: 3
+        },
+        devices: [
+            {
+                deviceId: {
+                    type: String
+                },
+                added: {
+                    type: Date,
+                    default: Date.now
+                },
+                lastUsed: {
+                    type: Date
+                }
+            }
+        ]
+    },
+    {
+        timestamps: true
+    }
+);
+
+Object.assign(subscriberSchema.statics, {Status});
+
 // Model
-const Subscriber = mongoose.model('Subscriber', subscriberSchema);
+const Subscriber = mongoose.model("Subscriber", subscriberSchema);
 
 // Module Exports
 module.exports = Subscriber;
